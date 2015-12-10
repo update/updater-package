2f32bad83cdad1b1878d3d61ef6d03471f6eb370
jonschlinkert <jon.schlinkert@sellside.com>
2015-12-10 13:25:42 -0500

    delete `verbiage` prop

--- updatefile.js
+++ updatefile.js
@@ -1,14 +1,27 @@
 'use strict';
 
 var normalize = require('normalize-pkg');
-
 var extend = require('extend-shallow');
 
 module.exports = function (app, base, env) {
   var options = base.option('pkg') || {};
 
-  base.onLoad(/package\.json/, function (file, next) {
-    normalize(file.json, options);
-    next();
+  options.verbiage = {
+    value: function (key, value, config) {
+      config.verb = value;
+      delete config[key];
+      return null;
+    }
+  };
+
+  base.onLoad(/package\.json/, function(file, next) {
+    base.log('updating package.json');
+    var pkg = extend({}, file.json);
+    try {
+      file.json = normalize(pkg, options);
+      next();
+    } catch (err) {
+      next(err);
+    }
   });
 };
