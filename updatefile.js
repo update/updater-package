e3147e7eeb05860f9aee9218e7ae2b1947de948d
jonschlinkert <jon.schlinkert@sellside.com>
2016-12-14 11:02:12 -0500

    refactor

--- updatefile.js
+++ updatefile.js
@@ -1,27 +1,160 @@
 'use strict';
 
-var normalize = require('normalize-pkg');
-var extend = require('extend-shallow');
+var path = require('path');
+var normalize = require('gulp-normalize-pkg');
+var utils = require('./utils');
 
-module.exports = function (app, base, env) {
-  var options = base.option('pkg') || {};
+module.exports = function(app) {
+  if (!utils.isValid(app, 'updater-package')) return;
 
-  options.verbiage = {
-    value: function (key, value, config) {
-      config.verb = value;
-      delete config[key];
-      return null;
+  app.use(utils.pkg());
+  app.option(app.base.options);
+  app.preWrite(/package.json/, function(file, next) {
+    var pkg = JSON.parse(file.contents);
+    pkg.files = pkg.files.filter(function(name) {
+      return !/license|readme\.md/i.test(name);
+    });
+
+    var deps = pkg.devDependencies;
+    if (deps && deps['verb-tag-jscomments']) {
+      delete deps['verb-tag-jscomments'];
+      delete deps['verb'];
+      pkg.devDependencies = deps;
     }
-  };
 
-  base.onLoad(/package\.json/, function(file, next) {
-    base.log('updating package.json');
-    var pkg = extend({}, file.json);
-    try {
-      file.json = normalize(pkg, options);
-      next();
-    } catch (err) {
-      next(err);
-    }
+    file.contents = new Buffer(JSON.stringify(pkg, null, 2).replace(/\s+$/, '\n'));
+    next();
   });
+
+  app.task('default', ['package-update']);
+
+  /**
+   * Normalizes and updates fields in package.json to use the latest [npm]() conventions,
+   * and baseline defaults defined in [normalize-pkg][]. Override any of the defaults
+   * by defining custom fields on `options.fields`. See [customization](#customization)
+   * for more details.
+   *
+   * ```sh
+   * update package
+   * ```
+   * @name package
+   * @api public
+   */
+
+  app.task('package-update', {silent: true}, function() {
+    var opts = utils.merge({}, app.option('pkg'));
+
+    return app.src('package.json', {cwd: app.cwd})
+      .pipe(createIndex(app))
+      .pipe(normalize(opts))
+      .pipe(owner())
+      .pipe(app.dest(app.cwd));
+  });
+
+  /**
+   * Ensures the `files` field in package.json has the value defined on the `main` property.
+   * Also aliased as `package:package-normalize` to prevent task naming conflicts when this is used
+   * as a plugin.
+   *
+   * ```sh
+   * update package:normalize
+   * ```
+   * @name package:normalize
+   * @api public
+   */
+
+  app.task('normalize', ['package-normalize']);
+  app.task('package-normalize', {silent: true}, function() {
+    var opts = utils.merge({}, app.option('pkg'));
+
+    return app.src('package.json', {cwd: app.cwd})
+      .pipe(normalize(opts))
+      .pipe(app.dest(app.cwd));
+  });
+
+  /**
+   * Updates the `files` and `main` fields in package.json to ensure it has the value defined on
+   * the `main` property. Also aliased as `package:package-files` to provide a semantic task
+   * name to use when this updater is used as a plugin.
+   *
+   * ```sh
+   * update package:index
+   * ```
+   * @name package:index
+   */
+
+  app.task('index', ['package-index']);
+  app.task('package-index', {silent: true}, function() {
+    return app.src('package.json', {cwd: app.cwd})
+      .pipe(createIndex(app))
+      .pipe(app.dest(app.cwd));
+  });
+
+  /**
+   * Runs [generate-package][] to create a new package.json in the current working directory
+   * or specified `--dest`.
+   *
+   * ```sh
+   * update package
+   * ```
+   * @name package
+   * @api public
+   */
+
+   app.task('new', function(cb) {
+     app.register('generate-package', require('generate-package'));
+     app.generate('generate-package', cb);
+   });
 };
+
+function newline(nl) {
+  if (typeof nl !== 'string') nl = '\n';
+  return utils.through.obj(function(file, enc, next) {
+    var str = file.contents.toString().replace(/\s+$/, '');
+    file.contents = new Buffer(str + nl);
+    next(null, file);
+  });
+}
+
+function createIndex(app) {
+  return utils.through.obj(function(file, enc, next) {
+    var pkg = JSON.parse(file.contents.toString());
+    var indexPath = path.resolve(app.cwd, 'index.js');
+
+    if (!pkg.hasOwnProperty('main') && !utils.exists(indexPath)) {
+      var configfile = utils.getFile(app.cwd);
+      if (configfile) {
+        var contents = `'use strict';\n\nmodule.exports = require('./${configfile}');`;
+        pkg.files = pkg.files || [];
+        if (pkg.files.indexOf('index.js') === -1) {
+          pkg.files.push('index.js');
+        }
+        if (pkg.files.indexOf(configfile + '.js') === -1) {
+          pkg.files.push(configfile + '.js');
+        }
+        pkg.main = 'index.js';
+        pkg.files.sort();
+
+        var index = new utils.File({path: indexPath, contents: new Buffer(contents)});
+        this.push(index);
+      }
+    }
+
+    file.contents = new Buffer(JSON.stringify(pkg, null, 2));
+    next(null, file);
+  });
+}
+
+function owner(options) {
+  return utils.through.obj(function(file, enc, next) {
+    if (file.basename !== 'package.json') {
+      next();
+      return;
+    }
+
+    var pkg = JSON.parse(file.contents.toString());
+    utils.updateOwner(pkg);
+    file.contents = new Buffer(JSON.stringify(pkg, null, 2));
+    next(null, file);
+  });
+}
---
+++ utils.js
@@ -0,0 +1,128 @@
+'use strict';
+
+var path = require('path');
+var utils = require('lazy-cache')(require);
+var fn = require;
+require = utils;
+
+/**
+ * Lazily required module dependencies
+ */
+
+require('base-pkg', 'pkg');
+require('fs-exists-sync', 'exists');
+require('mixin-deep', 'merge');
+require('isobject', 'isObject');
+require('is-valid-app', 'isValid');
+require('parse-github-url');
+require('parse-git-config', 'git');
+require('through2', 'through');
+require('vinyl', 'File');
+require = fn;
+
+// this is the old field name for `verb` in package.json.
+// It might not be found anywhere anymore, but just in case
+// we want to get rid of it
+utils.fields = {
+  verbiage: {
+    types: ['object'],
+    normalize: function(val, key, config, schema) {
+      if (typeof val !== 'undefined') {
+        config.verb = val;
+        schema.omit(key);
+      }
+    }
+  },
+  repository: {
+    types: ['object', 'string'],
+    normalize: function(val, key, config, schema) {
+      console.log(arguments)
+      return val;
+    }
+  },
+};
+
+utils.fixRepo = function(app, str) {
+  var owner = utils.getOwner(app);
+
+};
+
+utils.getFile = function(cwd) {
+  var files = ['updatefile', 'generator', 'assemblefile', 'verbfile'];
+  var len = files.length;
+  var idx = -1;
+  while (++idx < len) {
+    var file = files[idx];
+    var fp = path.resolve(cwd, file);
+    if (utils.exists(fp + '.js')) {
+      return file;
+    }
+  }
+};
+
+utils.getRepo = function(pkg) {
+  var repo = pkg.repository;
+  if (utils.isObject(repo)) {
+    var parsed = parseGithubUrl(repo.url);
+    repo = parsed.repo;
+  }
+  pkg.repository = repo;
+  return repo;
+};
+
+utils.updateOwner = function(pkg) {
+  var name = pkg.name;
+  var repo = utils.getRepo(pkg);
+  var segs = repo.split('/');
+  if (segs.length !== 2) {
+    return;
+  }
+
+  var owner = segs[0];
+  var orig = owner;
+
+  if (/^base-/.test(name)) {
+    owner = 'node-base';
+  } else if (/^assemble-/.test(name)) {
+    owner = 'assemble';
+  } else if (/^generate-/.test(name)) {
+    owner = 'generate';
+  } else if (/^(helper|handlebars-helper)-/.test(name)) {
+    owner = 'helpers';
+  } else if (/^sellside-/.test(name)) {
+    owner = 'sellside';
+  } else if (/^toolkit-/.test(name)) {
+    owner = 'node-toolkit';
+  } else if (/^updater-/.test(name)) {
+    owner = 'update';
+  } else if (/^verb-/.test(name)) {
+    owner = 'verbose';
+  }
+
+  // if `owner` isn't what we originally detected AND
+  // the user that is currently running `update` is
+  // the value we originally detected, then it's a good
+  // bet the "owner" values are incorrect, so let's fix
+  // them.
+  if (orig !== owner && utils.currentUser() === orig) {
+    pkg.repository = `${owner}/${segs[1]}`;
+    if (pkg.bugs && pkg.bugs.url) {
+      pkg.bugs.url = pkg.bugs.url.replace(orig, owner);
+    }
+
+    if (pkg.homepage && /github\.com\//.test(pkg.homepage)) {
+      pkg.homepage = pkg.homepage.replace(orig, owner);
+    }
+  }
+};
+
+utils.currentUser = function() {
+  var config = utils.git.sync('global') || {};
+  return config.user && config.user.name;
+};
+
+/**
+ * Expose `utils` modules
+ */
+
+module.exports = utils;
